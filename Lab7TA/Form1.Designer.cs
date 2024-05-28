1c94ecef51c8d85ab38eb665d069193eec594c9c
Diana R <romanuk13082006@gmail.com>
2024-05-28 15:20:40 +0300

    Add project files.

---
+++ Lab7TA/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Lab7TA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 260);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(869, 476);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Honeydew;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(141, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Honeydew;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(389, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Honeydew;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(645, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(191, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.label1.Location = new System.Drawing.Point(157, 56);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(164, 44);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Викопати печеру \r\nз кількістю злитків";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.label2.Location = new System.Drawing.Point(407, 56);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(164, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Засипати печеру\r\nз кількістю злитків";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.label3.Location = new System.Drawing.Point(631, 56);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(228, 44);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Показати шлях до печери\r\nз кількістю злитків";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Khaki;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(169, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 50);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Додати";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Khaki;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.button2.Location = new System.Drawing.Point(421, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 50);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Видалити";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Khaki;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.button3.Location = new System.Drawing.Point(682, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 50);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Знайти";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.ShowPath_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Honeydew;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.label4.Location = new System.Drawing.Point(59, 790);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(869, 78);
+            this.label4.TabIndex = 10;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(996, 1023);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab7TA/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab7TA
+{
+    public partial class Form1 : Form
+    {
+        private BinaryTree _BinaryTree;
+        private Bitmap _Binarybitmap;
+        private Graphics _Binarygraphics;
+        public Form1()
+        {
+            InitializeComponent();
+            _BinaryTree = new BinaryTree();
+            _Binarybitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            _Binarygraphics = Graphics.FromImage(_Binarybitmap);
+            InitialTree();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void InitialTree()
+        {
+            int[] nodes = { 42, 28, 21, 17, 31, 30, 35, 62, 59, 57, 60, 71, 67, 82};
+
+            foreach (int value in nodes)
+            {
+                _BinaryTree.Add(value);
+            }
+
+            DrawTree();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int element))
+            {
+                MessageBox.Show("Будь ласка, введіть коректне значення " +
+                    "кількості злитків!",
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (_BinaryTree.Search(element))
+            {
+                MessageBox.Show("Печера, що містить вказану кількість злитків вже існує!",
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            _BinaryTree.Add(element);
+            DrawTree();
+            textBox1.Clear();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox2.Text, out int element))
+            {
+                MessageBox.Show("Будь ласка, введіть коректне значення " +
+                    "кількості злитків!",
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (!(_BinaryTree.Search(element)))
+            {
+                MessageBox.Show("Не існує печери, що містить вказану кількість злитків!",
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            _BinaryTree.Remove(element);
+            DrawTree();
+            textBox2.Clear();
+        }
+
+        private void ShowPath_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox3.Text, out int element))
+            {
+                MessageBox.Show("Будь ласка, введіть коректне значення елемента" +
+                    "кількості злитків!",
+                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!(_BinaryTree.Search(element)))
+            {
+                MessageBox.Show($"Шуканої печери з кількістю злитків {element} не існує!" +
+                    $" Незабаром її буде створено.",
+                    "Інформація", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            List<int> path = _BinaryTree.FindPath(element);
+            string pathString = string.Join(" -> ", path);
+            DrawTree();
+            label4.Text = ($"Шлях до заданого скарбу: {pathString}");
+            textBox3.Clear();
+        }
+
+        private void DrawTree()
+        {
+            _Binarygraphics.Clear(Color.CadetBlue);
+            DrawNode(_BinaryTree.root, 300, 20, 150);
+            pictureBox1.Image = _Binarybitmap;
+        }
+
+        private void DrawNode(TreeNode node, float x, float y, float dx)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            float ellipseWidth = 32; 
+            float ellipseHeight = 32;
+            Pen ellipsePen = new Pen(Color.Black, 2);
+            _Binarygraphics.DrawEllipse(ellipsePen, x, y, ellipseWidth, ellipseHeight);
+            _Binarygraphics.FillEllipse(Brushes.White, x, y, ellipseWidth, ellipseHeight);
+
+            SizeF textSize = _Binarygraphics.MeasureString(node.Key.ToString(), 
+                new Font("Cascadia Mono", 12));
+
+            float textX = x + (ellipseWidth - textSize.Width) / 2;
+            float textY = y + (ellipseHeight - textSize.Height) / 2;
+
+            _Binarygraphics.DrawString(node.Key.ToString(), new Font("Cascadia Mono", 12),
+                Brushes.Black, textX, textY);
+            Pen widePen = new Pen(Color.Black, 1);
+
+            if (node.left != null)
+            {
+                _Binarygraphics.DrawLine(widePen, x + ellipseWidth / 2, y + ellipseHeight,
+                    x - dx + ellipseWidth / 2, y + ellipseHeight + 20);
+                DrawNode(node.left, x - dx, y + 52, dx / 2);
+            }
+
+            if (node.right != null)
+            {
+                _Binarygraphics.DrawLine(widePen, x + ellipseWidth / 2, y + ellipseHeight,
+                    x + dx + ellipseWidth / 2, y + ellipseHeight + 20);
+                DrawNode(node.right, x + dx, y + 52, dx / 2);
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
